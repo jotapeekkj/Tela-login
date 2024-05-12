0e176144c16da1f54beb4b9d02678013e2a4bd0c
João Pedro <jotapefaustino7@gmail.com>
2024-05-12 18:23:06 -0300

    Validação básica do formulário com JavaScript e interatividade com o usuário

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-entrar').addEventListener('click', function() {
+    var email = document.getElementById('email');
+    var senha = document.getElementById('senha');
+    var emailError = document.getElementById('email-error');
+    var senhaError = document.getElementById('senha-error');
+
+    emailError.textContent = '';
+    senhaError.textContent = '';
+
+    if (email.value.trim() === '' || senha.value.trim() === '') {
+        alert('Por favor, preencha todos os campos.');
+    } else if (!/\S+@\S+\.\S+/.test(email.value)) {
+        emailError.textContent = 'Por favor, insira um endereço de e-mail válido.';
+        email.classList.add('invalid');
+    } else if (senha.value.length < 8) {
+        senhaError.textContent = 'A senha deve conter no mínimo 8 caracteres';
+        senha.classList.add('invalid');
+    } else {
+        email.classList.remove('invalid');
+        senha.classList.remove('invalid');
+        alert('Campos preenchidos. Enviando formulário...');
+    }
+});
